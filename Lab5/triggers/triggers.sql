818fd350e21e90509ffd8820bacf4ed6de979819
Tivlas <tima021003tima@gmail.com>
2023-11-11 14:02:13 +0300

    Правки в триггерах лаба 5.

--- Lab5/triggers/triggers.sql
+++ Lab5/triggers/triggers.sql
@@ -1,12 +1,17 @@
 -- @block
 DROP TRIGGER IF EXISTS update_cart_price_trigger ON cart_item;
+
 DROP TRIGGER IF EXISTS insert_cart_for_new_client ON client;
+
 DROP TRIGGER IF EXISTS copy_cart_items_to_order_items_trigger ON "order";
 
 -- @block
-CREATE TRIGGER update_cart_price_trigger
+CREATE TRIGGER after_insert_cart_item_trigger
 AFTER
-INSERT OR DELETE ON cart_item FOR EACH ROW EXECUTE PROCEDURE update_cart_price();
+INSERT ON cart_item FOR EACH ROW EXECUTE PROCEDURE update_cart_price();
+
+CREATE TRIGGER after_delete_cart_item_trigger
+AFTER DELETE ON cart_item FOR EACH ROW EXECUTE PROCEDURE update_cart_price();
 
 CREATE TRIGGER insert_cart_for_new_client
 AFTER
