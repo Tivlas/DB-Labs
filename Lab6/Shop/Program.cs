43b9f36d5aaf99e807e0efb2c41999bef74ccc83
Tivlas <tima021003tima@gmail.com>
2023-11-26 23:09:40 +0300

    Отзывы.

--- Lab6/Shop/Program.cs
+++ Lab6/Shop/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IDiscountService, DiscountService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
 
 builder.Services.AddAuthentication().AddCookie("Cookies");
 builder.Services.ConfigLogger();
